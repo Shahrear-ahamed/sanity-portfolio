9b7645095f3a4b0697f9b88bbcd9dde446293040
Shahrear-ahamed <its.shahrear@gmail.com>
2023-11-27 01:08:25 +0600

    [DONE] add right click disable

--- src/lib/providers.tsx
+++ src/lib/providers.tsx
@@ -1,14 +1,27 @@
 // app/providers.tsx
 "use client";
+import { useEffect } from "react";
 
 import { NextUIProvider } from "@nextui-org/react";
 import { Toaster } from "react-hot-toast";
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    const handleContextMenu = (e: any) => {
+      e.preventDefault();
+    };
+
+    document.addEventListener("contextmenu", handleContextMenu);
+
+    return () => {
+      document.removeEventListener("contextmenu", handleContextMenu);
+    };
+  }, []);
+
   return (
     <NextUIProvider>
       {children}
-      <Toaster position="bottom-right" reverseOrder={false}/>
+      <Toaster position="bottom-right" reverseOrder={false} />
     </NextUIProvider>
   );
 }
